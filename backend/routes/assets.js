8c757e1b990e751a68a8c2e68cba1c857321113e
yTomi1337 <laniocha123@gmail.com>
2024-12-23 13:16:26 +0100

    Added backend routes to /asset/:id (.get)

--- backend/routes/assets.js
+++ backend/routes/assets.js
@@ -7,4 +7,10 @@ router.get('/assets', function(req, res){
     Asset.findAll({}).then((asset) => {res.send(asset)})
 })
 
+router.get('/assets/:id', function(req, res){
+    Asset.findByPk(Number(req.params.id)).then((asset) => {
+        res.send(asset)
+    })
+})
+
 module.exports = router;
