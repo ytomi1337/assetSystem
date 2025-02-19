7c82a0c2a94191c0c8c481af3fdd95ce8b068240
yTomi1337 <laniocha123@gmail.com>
2025-02-19 14:21:38 +0000

    backend endpoint "post" for filters

--- backend/routes/assets.js
+++ backend/routes/assets.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Asset = require('../models/assets.js');
-const { Sequelize } = require('sequelize');
+const { Sequelize, where, Op } = require('sequelize');
 const { body, validationResult } = require('express-validator')
 
 
@@ -71,6 +71,60 @@ router.post('/assets', [
     })
 })
 
+router.post('/assets/filter', function(req, res){
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const sortKey = req.query.sortKey || 'id'
+    const sortValue = req.query.sortValue || 'asc'
+    const filter = {}
+    if (req.body.name){
+        filter.name = req.body.name
+    }
+    if (req.body.it_num){
+        filter.it_num = req.body.it_num
+    }
+    if (req.body.serialnum){
+        filter.serialnum = req.body.serialnum
+    }
+    if (req.body.user_new && req.body.user_new.length){
+        filter.user_new = {
+            [Op.in]: req.body.user_new
+        }
+    }
+    if (req.body.localization && req.body.localization.length){
+        filter.localization = {
+            [Op.in]: req.body.localization
+        }
+    }
+    if (req.body.category && req.body.category.length){
+        filter.category = {
+            [Op.in]: req.body.category
+        }
+
+    }
+    if (req.body.status && req.body.status.length){
+        filter.status = {
+            [Op.in]: req.body.status
+        }
+    }
+    Promise.all([
+        Asset.count({
+            where: filter
+        }),
+        Asset.findAll({
+            offset: (page -1) * limit,
+            limit: (limit),
+            order: [ [sortKey, sortValue] ],
+            where: filter
+        })
+    ]).then(([count, assets]) => {
+        res.send({
+            count: count,
+            assets: assets
+        })
+    })
+})
+
 router.delete('/assets/:id', function(req, res){
 
     Asset.destroy({
@@ -102,6 +156,7 @@ router.patch('/assets/:id', async(req, res) => {
         res.status(500).json({message: "Wystapił błąd podczas aktualizacji", error})
     }
 })
+
 
 
 module.exports = router;
