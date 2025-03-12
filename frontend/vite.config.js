52249d6b2ac85d714c41510b5fbaea0d547722f9
yTomi1337 <laniocha123@gmail.com>
2025-03-12 13:07:23 +0000

    improved z-index on website

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
